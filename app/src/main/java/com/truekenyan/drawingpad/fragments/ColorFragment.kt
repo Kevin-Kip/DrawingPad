b2515e1ba8dd3992292278cfc9ea5fe44fd3a9b2
Kevin-Kip <masterfork5@gmail.com>
2018-12-23 10:22:27 +0300

    Initialized buttons correctly

--- app/src/main/java/com/truekenyan/drawingpad/fragments/ColorFragment.kt
+++ app/src/main/java/com/truekenyan/drawingpad/fragments/ColorFragment.kt
@@ -6,13 +6,12 @@ import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 import com.truekenyan.drawingpad.R
 import com.truekenyan.drawingpad.interfaces.OnValueChanged
 
-import butterknife.ButterKnife
-import butterknife.OnClick
-import butterknife.Unbinder
+import com.truekenyan.drawingpad.utilities.Commons
 
 /**
  * Created by password
@@ -22,56 +21,83 @@ import butterknife.Unbinder
 class ColorFragment : DialogFragment() {
 
     private var valueChanged: OnValueChanged? = null
+    private var buttonBlack: Button? = null
+    private var buttonBlue: Button? = null
+    private var buttonGreen: Button? = null
+    private var buttonGrey: Button? = null
+    private var buttonOrange: Button? = null
+    private var buttonPink: Button? = null
+    private var buttonRed: Button? = null
+    private var buttonWhite: Button? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_color, container, false)
+        buttonBlack = view.findViewById(R.id.button_black)
+        buttonBlue = view.findViewById(R.id.button_blue)
+        buttonGreen = view.findViewById(R.id.button_green)
+        buttonGrey = view.findViewById(R.id.button_grey)
+        buttonOrange = view.findViewById(R.id.button_orange)
+        buttonPink = view.findViewById(R.id.button_pink)
+        buttonRed = view.findViewById(R.id.button_red)
+        buttonWhite = view.findViewById(R.id.button_white)
+
+        buttonBlack!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonBlue!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonGreen!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonGrey!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonOrange!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonPink!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonRed!!.setOnClickListener(onViewClicked(Commons.BLACK))
+        buttonWhite!!.setOnClickListener(onViewClicked(Commons.BLACK))
+
         dialog.setTitle("Pick a Color")
         return view
     }
 
-    @OnClick(R.id.button_black, R.id.button_blue, R.id.button_green, R.id.button_grey, R.id.button_orange, R.id.button_pink, R.id.button_red, R.id.button_white)
-    fun onViewClicked(view: View) {
-        val selectedColor: Int
-        when (view.id) {
-            R.id.button_black -> {
-                selectedColor = -0x1000000
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_blue -> {
-                selectedColor = -0xf2b85f
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_green -> {
-                selectedColor = -0xffb2c0
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_grey -> {
-                selectedColor = -0x616162
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_orange -> {
-                selectedColor = -0xa80e9
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_pink -> {
-                selectedColor = -0x23f0be
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_red -> {
-                selectedColor = -0x48e3e4
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
-            }
-            R.id.button_white -> {
-                selectedColor = -0x1
-                valueChanged!!.onColorChanged(selectedColor)
-                dismiss()
+    fun onViewClicked(action: String): View.OnClickListener {
+        var selectedColor: Int
+        return View.OnClickListener {
+            when (action) {
+                Commons.BLACK -> {
+                    selectedColor = -0x1000000
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.BLUE -> {
+                    selectedColor = -0xf2b85f
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.GREEN -> {
+                    selectedColor = -0xffb2c0
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.GREY -> {
+                    selectedColor = -0x616162
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.ORANGE -> {
+                    selectedColor = -0xa80e9
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.PINK -> {
+                    selectedColor = -0x23f0be
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.RED -> {
+                    selectedColor = -0x48e3e4
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
+                Commons.WHITE -> {
+                    selectedColor = -0x1
+                    valueChanged!!.onColorChanged(selectedColor)
+                    dismiss()
+                }
             }
         }
     }
