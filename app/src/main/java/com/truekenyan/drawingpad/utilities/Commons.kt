446fc1c0395d0cfd047eef657ca088c4f5e5e609
Kevin-Kip <masterfork5@gmail.com>
2018-12-23 10:21:59 +0300

    Created common variables file

--- app/src/main/java/com/truekenyan/drawingpad/utilities/Commons.kt
+++ app/src/main/java/com/truekenyan/drawingpad/utilities/Commons.kt
@@ -1,4 +1,14 @@
 package com.truekenyan.drawingpad.utilities
 
 object Commons {
+    const val CANCEL = "CANCEL"
+    const val OK = "OK"
+    const val BLUE = "blue"
+    const val BLACK = "black"
+    const val WHITE = "white"
+    const val RED = "red"
+    const val GREEN = "green"
+    const val GREY = "grey"
+    const val ORANGE = "orange"
+    const val PINK = "pink"
 }
